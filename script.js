8aac7b365087925e6264b7679020817782c07674
gi-nascimento <gigicasha@gmail.com>
2022-12-03 03:14:47 -0300

    Trabalho
    atualizações
    
    Co-Authored-By: laurooo <108877034+laurooo@users.noreply.github.com>

--- script.js
+++ script.js
@@ -65,44 +65,238 @@ spanDados.onclick = function() {
 }
 
 
-// identifica o elemento 
-
-// esconde
-var dec1970 = document.getElementsByClassName("dec1970");
-dec1970[0].style.display = "none";
-var dec1980 = document.getElementsByClassName("dec1980");
-dec1980[0].style.display = "none";
-dec1980[1].style.display = "none";
-dec1980[2].style.display = "none";
-var dec1990 = document.getElementsByClassName("dec1990");
-dec1990[0].style.display = "none";
-dec1990[1].style.display = "none";
-dec1990[2].style.display = "none";
-dec1990[3].style.display = "none";
-dec1990[4].style.display = "none";
-var dec2000 = document.getElementsByClassName("dec2000");
-dec2000[0].style.display = "none";
-dec2000[1].style.display = "none";
-var dec2010 = document.getElementsByClassName("dec2010");
-dec2010[0].style.display = "none";
-dec2010[1].style.display = "none";
-dec2010[2].style.display = "none";
-dec2010[3].style.display = "none";
-dec2010[4].style.display = "none";
-var dec2020 = document.getElementsByClassName("dec2020");
-dec2020[0].style.display = "none";
-dec2020[1].style.display = "none";
+  // identifica os elementos
+  var dec1970 = document.getElementsByClassName("dec1970");
+  var dec1980 = document.getElementsByClassName("dec1980");
+  var dec1990 = document.getElementsByClassName("dec1990");
+  var dec2000 = document.getElementsByClassName("dec2000");
+  var dec2010 = document.getElementsByClassName("dec2010");
+  var dec2020 = document.getElementsByClassName("dec2020");
+// oculta os elementos
+  dec1970[0].style.display = "none";
+  dec1980[0].style.display = "none";
+  dec1980[1].style.display = "none";
+  dec1980[2].style.display = "none";
+  dec1990[0].style.display = "none";
+  dec1990[1].style.display = "none";
+  dec1990[2].style.display = "none";
+  dec1990[3].style.display = "none";
+  dec1990[4].style.display = "none";
+  dec2000[0].style.display = "none";
+  dec2000[1].style.display = "none";
+  dec2010[0].style.display = "none";
+  dec2010[1].style.display = "none";
+  dec2010[2].style.display = "none";
+  dec2010[3].style.display = "none";
+  dec2010[4].style.display = "none";
+  dec2020[0].style.display = "none";
+  dec2020[1].style.display = "none";
 
 
 // radio decada 1970
 var radio1 = document.getElementById("radio1");
 radio1.onclick = function() {
-  // mostra a decada 70
-  dec1970[0].style.display = "block";
-  dec1970[1].style.display = "block";
-  dec1970[2].style.display = "block";
-  // oculta as demais
+  var dec1970 = document.getElementsByClassName("dec1970");
+  var dec1980 = document.getElementsByClassName("dec1980");
+  var dec1990 = document.getElementsByClassName("dec1990");
+  var dec2000 = document.getElementsByClassName("dec2000");
+  var dec2010 = document.getElementsByClassName("dec2010");
+  var dec2020 = document.getElementsByClassName("dec2020");
+  // oculta os elementos
+  dec1970[0].style.display = "none";
   dec1980[0].style.display = "none";
   dec1980[1].style.display = "none";
   dec1980[2].style.display = "none";
+  dec1990[0].style.display = "none";
+  dec1990[1].style.display = "none";
+  dec1990[2].style.display = "none";
+  dec1990[3].style.display = "none";
+  dec1990[4].style.display = "none";
+  dec2000[0].style.display = "none";
+  dec2000[1].style.display = "none";
+  dec2010[0].style.display = "none";
+  dec2010[1].style.display = "none";
+  dec2010[2].style.display = "none";
+  dec2010[3].style.display = "none";
+  dec2010[4].style.display = "none";
+  dec2020[0].style.display = "none";
+  dec2020[1].style.display = "none";
+  // mostra a decada 70
+  dec1970[0].style.display = "block";
+}
+
+
+// radio decada 1980
+var radio2 = document.getElementById("radio2");
+radio2.onclick = function() {
+  var dec1970 = document.getElementsByClassName("dec1970");
+  var dec1980 = document.getElementsByClassName("dec1980");
+  var dec1990 = document.getElementsByClassName("dec1990");
+  var dec2000 = document.getElementsByClassName("dec2000");
+  var dec2010 = document.getElementsByClassName("dec2010");
+  var dec2020 = document.getElementsByClassName("dec2020");
+  // oculta os elementos
+  dec1970[0].style.display = "none";
+  dec1980[0].style.display = "none";
+  dec1980[1].style.display = "none";
+  dec1980[2].style.display = "none";
+  dec1990[0].style.display = "none";
+  dec1990[1].style.display = "none";
+  dec1990[2].style.display = "none";
+  dec1990[3].style.display = "none";
+  dec1990[4].style.display = "none";
+  dec2000[0].style.display = "none";
+  dec2000[1].style.display = "none";
+  dec2010[0].style.display = "none";
+  dec2010[1].style.display = "none";
+  dec2010[2].style.display = "none";
+  dec2010[3].style.display = "none";
+  dec2010[4].style.display = "none";
+  dec2020[0].style.display = "none";
+  dec2020[1].style.display = "none";
+  // mostra a decada 80
+  dec1980[0].style.display = "block";
+  dec1980[1].style.display = "block";
+  dec1980[2].style.display = "block";
+}
+
+
+// radio decada 1990
+var radio3 = document.getElementById("radio3");
+radio3.onclick = function() {
+  var dec1970 = document.getElementsByClassName("dec1970");
+  var dec1980 = document.getElementsByClassName("dec1980");
+  var dec1990 = document.getElementsByClassName("dec1990");
+  var dec2000 = document.getElementsByClassName("dec2000");
+  var dec2010 = document.getElementsByClassName("dec2010");
+  var dec2020 = document.getElementsByClassName("dec2020");
+  // oculta os elementos
+  dec1970[0].style.display = "none";
+  dec1980[0].style.display = "none";
+  dec1980[1].style.display = "none";
+  dec1980[2].style.display = "none";
+  dec1990[0].style.display = "none";
+  dec1990[1].style.display = "none";
+  dec1990[2].style.display = "none";
+  dec1990[3].style.display = "none";
+  dec1990[4].style.display = "none";
+  dec2000[0].style.display = "none";
+  dec2000[1].style.display = "none";
+  dec2010[0].style.display = "none";
+  dec2010[1].style.display = "none";
+  dec2010[2].style.display = "none";
+  dec2010[3].style.display = "none";
+  dec2010[4].style.display = "none";
+  dec2020[0].style.display = "none";
+  dec2020[1].style.display = "none";
+  // mostra a decada 90
+  dec1990[0].style.display = "block";
+  dec1990[1].style.display = "block";
+  dec1990[2].style.display = "block";
+  dec1990[3].style.display = "block";
+  dec1990[4].style.display = "block";
+}
+
+
+// radio decada 2000
+var radio4 = document.getElementById("radio4");
+radio4.onclick = function() {
+  var dec1970 = document.getElementsByClassName("dec1970");
+  var dec1980 = document.getElementsByClassName("dec1980");
+  var dec1990 = document.getElementsByClassName("dec1990");
+  var dec2000 = document.getElementsByClassName("dec2000");
+  var dec2010 = document.getElementsByClassName("dec2010");
+  var dec2020 = document.getElementsByClassName("dec2020");
+  // oculta os elementos
+  dec1970[0].style.display = "none";
+  dec1980[0].style.display = "none";
+  dec1980[1].style.display = "none";
+  dec1980[2].style.display = "none";
+  dec1990[0].style.display = "none";
+  dec1990[1].style.display = "none";
+  dec1990[2].style.display = "none";
+  dec1990[3].style.display = "none";
+  dec1990[4].style.display = "none";
+  dec2000[0].style.display = "none";
+  dec2000[1].style.display = "none";
+  dec2010[0].style.display = "none";
+  dec2010[1].style.display = "none";
+  dec2010[2].style.display = "none";
+  dec2010[3].style.display = "none";
+  dec2010[4].style.display = "none";
+  dec2020[0].style.display = "none";
+  dec2020[1].style.display = "none";
+  // mostra a decada 2000
+  dec2000[0].style.display = "block";
+  dec2000[1].style.display = "block";
+}
+
+
+// radio decada 2010
+var radio5 = document.getElementById("radio5");
+radio5.onclick = function() {
+  var dec1970 = document.getElementsByClassName("dec1970");
+  var dec1980 = document.getElementsByClassName("dec1980");
+  var dec1990 = document.getElementsByClassName("dec1990");
+  var dec2000 = document.getElementsByClassName("dec2000");
+  var dec2010 = document.getElementsByClassName("dec2010");
+  var dec2020 = document.getElementsByClassName("dec2020");
+  // oculta os elementos
+  dec1970[0].style.display = "none";
+  dec1980[0].style.display = "none";
+  dec1980[1].style.display = "none";
+  dec1980[2].style.display = "none";
+  dec1990[0].style.display = "none";
+  dec1990[1].style.display = "none";
+  dec1990[2].style.display = "none";
+  dec1990[3].style.display = "none";
+  dec1990[4].style.display = "none";
+  dec2000[0].style.display = "none";
+  dec2000[1].style.display = "none";
+  dec2010[0].style.display = "none";
+  dec2010[1].style.display = "none";
+  dec2010[2].style.display = "none";
+  dec2010[3].style.display = "none";
+  dec2010[4].style.display = "none";
+  dec2020[0].style.display = "none";
+  dec2020[1].style.display = "none";
+  // mostra a decada 2010
+  dec2010[0].style.display = "block";
+  dec2010[1].style.display = "block";
+  dec2010[2].style.display = "block";
+  dec2010[3].style.display = "block";
+  dec2010[4].style.display = "block";
+}
+
+// radio decada 2020
+var radio6 = document.getElementById("radio6");
+radio6.onclick = function() {
+  var dec1970 = document.getElementsByClassName("dec1970");
+  var dec1980 = document.getElementsByClassName("dec1980");
+  var dec1990 = document.getElementsByClassName("dec1990");
+  var dec2000 = document.getElementsByClassName("dec2000");
+  var dec2010 = document.getElementsByClassName("dec2010");
+  var dec2020 = document.getElementsByClassName("dec2020");
+  // oculta os elementos
+  dec1970[0].style.display = "none";
+  dec1980[0].style.display = "none";
+  dec1980[1].style.display = "none";
+  dec1980[2].style.display = "none";
+  dec1990[0].style.display = "none";
+  dec1990[1].style.display = "none";
+  dec1990[2].style.display = "none";
+  dec1990[3].style.display = "none";
+  dec1990[4].style.display = "none";
+  dec2000[0].style.display = "none";
+  dec2000[1].style.display = "none";
+  dec2010[0].style.display = "none";
+  dec2010[1].style.display = "none";
+  dec2010[2].style.display = "none";
+  dec2010[3].style.display = "none";
+  dec2010[4].style.display = "none";
+  dec2020[0].style.display = "none";
+  dec2020[1].style.display = "none";
+  // mostra a decada 2020
+  dec2020[0].style.display = "block";
+  dec2020[1].style.display = "block";
 }
